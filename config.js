4bcb50f2680fc4da19af016b5f577f65a33f291f
Whtsapp23 <Adeeshaeshan773@gmail.com>
2024-12-11 18:01:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,20 +5,22 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", /* you sessin_id */
+SESSION_ID: process.env.SESSION_ID || "KFsGRCyQ#7zz0RNDm7xhZrjODXyk_F3Q5of13dkhMZm-OWW7uHFE", /* you sessin_id */
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ak5sns.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM ANGEL MD IS AVAILABLE FOR YOUR HELP",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/sHJVhDj",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hy I Am Adeesha Eshan 👀😂
+
+"වචන අඩුවෙන් කෙරෙන්න ඔන දේ කෙටියෙන් Typeing එකක් දපන්😏💔"",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "ANGEL MD",
+BOT_NAME: process.env.BOT_NAME || "Adeesha eshan",
 LANG: process.env.LANG || "SI"
 };
